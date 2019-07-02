a884e66f941e1be51bac775b759d59bb0b85a3f6
Simon B <simonbukin@gmail.com>
2019-07-02 15:11:12 -0700

    Delete config.py

--- config.py
+++
@@ -1,3 +0,0 @@
-client_id = '527435610710-meer2c7m0in16mvb256unrg31noocgd6.apps.googleusercontent.com'
-client_secret = '9ldjOOV6VBqwoRXxE7N8Fd4y'
-spreadsheet_id = '1sjY3UQHTSYSOQ3N1dYDAw9g-YhDslb72NsPExPEv9Qw'
